05ba769e0366a58500938640ff1a41c8ade7015f
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-12 14:19:45 +0300

    Finish mission reducer tests

---
+++ src/redux/__tests__/mission.test.js
@@ -0,0 +1,44 @@
+import reducer, { getMissions, joinMission } from '../missions/missions';
+
+test('should return the initial state', () => {
+  expect(reducer(undefined, {})).toEqual([]);
+});
+
+test(' getMission should return updated state with missions', () => {
+  const previousState = [];
+  const missions = [
+    {
+      id: 1,
+      name: 'Test mission',
+      description: 'This is a test Mission',
+      joined: false,
+    },
+  ];
+  expect(reducer(previousState, getMissions(missions))).toEqual([
+    {
+      id: 1,
+      name: 'Test mission',
+      description: 'This is a test Mission',
+      joined: false,
+    },
+  ]);
+});
+
+test('Join mission should update the joined status of a mission', () => {
+  const previousState = [
+    {
+      id: 1,
+      name: 'Test mission',
+      description: 'This is a test Mission',
+      joined: false,
+    },
+  ];
+  expect(reducer(previousState, joinMission(1))).toEqual([
+    {
+      id: 1,
+      name: 'Test mission',
+      description: 'This is a test Mission',
+      joined: true,
+    },
+  ]);
+});
