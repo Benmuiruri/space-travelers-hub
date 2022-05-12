d9d0a65c18506910ebb2d62c2e050463b313bcf4
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-12 13:40:55 +0300

    Fix count bug in component return

--- src/components/Pages/Profile.js
+++ src/components/Pages/Profile.js
@@ -10,35 +10,27 @@ const Profile = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     // @ts-ignore
-    dispatch(getAllMissions());
+    if (missions.length === 0) dispatch(getAllMissions());
   }, []);
-
-  if (missions.length) {
-    return (
-      <section className={classes.profileContainer}>
-        <h2>My Profile</h2>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Joined Missions</th>
-              <th>Reserved Rockets</th>
-            </tr>
-          </thead>
-          <tbody>
-            {missions.map((mission) => (
-              <tr key={mission.id}>
-                <td>{mission.name}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </section>
-    );
-  }
   return (
-    <>
-      <p>You have not joined any missions or reserved any rockets. </p>
-    </>
+    <section className={classes.profileContainer}>
+      <h2>My Profile</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Joined Missions</th>
+            <th>Reserved Rockets</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.id}>
+              <td>{mission.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </section>
   );
 };
 export default Profile;
