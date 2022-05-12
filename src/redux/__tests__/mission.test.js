c2f4a82cea757e0869a0e9035fc22a9137389da4
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-12 15:52:43 +0300

    Wrap mission store tests in describe block

--- src/redux/__tests__/mission.test.js
+++ src/redux/__tests__/mission.test.js
@@ -1,44 +1,46 @@
 import reducer, { getMissions, joinMission } from '../missions/missions';
 
-test('should return the initial state', () => {
-  expect(reducer(undefined, {})).toEqual([]);
-});
+describe('Testing Mission store state', () => {
+  test('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual([]);
+  });
 
-test(' getMission should return updated state with missions', () => {
-  const previousState = [];
-  const missions = [
-    {
-      id: 1,
-      name: 'Test mission',
-      description: 'This is a test Mission',
-      joined: false,
-    },
-  ];
-  expect(reducer(previousState, getMissions(missions))).toEqual([
-    {
-      id: 1,
-      name: 'Test mission',
-      description: 'This is a test Mission',
-      joined: false,
-    },
-  ]);
-});
+  test(' getMission should return updated state with missions', () => {
+    const previousState = [];
+    const missions = [
+      {
+        id: 1,
+        name: 'Test mission',
+        description: 'This is a test Mission',
+        joined: false,
+      },
+    ];
+    expect(reducer(previousState, getMissions(missions))).toEqual([
+      {
+        id: 1,
+        name: 'Test mission',
+        description: 'This is a test Mission',
+        joined: false,
+      },
+    ]);
+  });
 
-test('Join mission should update the joined status of a mission', () => {
-  const previousState = [
-    {
-      id: 1,
-      name: 'Test mission',
-      description: 'This is a test Mission',
-      joined: false,
-    },
-  ];
-  expect(reducer(previousState, joinMission(1))).toEqual([
-    {
-      id: 1,
-      name: 'Test mission',
-      description: 'This is a test Mission',
-      joined: true,
-    },
-  ]);
+  test('Join mission should update the joined status of a mission', () => {
+    const previousState = [
+      {
+        id: 1,
+        name: 'Test mission',
+        description: 'This is a test Mission',
+        joined: false,
+      },
+    ];
+    expect(reducer(previousState, joinMission(1))).toEqual([
+      {
+        id: 1,
+        name: 'Test mission',
+        description: 'This is a test Mission',
+        joined: true,
+      },
+    ]);
+  });
 });
