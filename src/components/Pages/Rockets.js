94f1eb9b71c664796fa89df594c03fb3f3ab6c5a
Regiss Mukubiza <regiss.steven@gmail.com>
2022-05-14 10:18:27 +0200

    rocket page

--- src/components/Pages/Rockets.js
+++ src/components/Pages/Rockets.js
@@ -1,2 +1,28 @@
-const Rockets = () => <p>This is the rockets page </p>;
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Container from 'react-bootstrap/Container';
+import styles from '../rockets/Rockets.module.css';
+import RocketItem from '../rockets/elementRockets';
+
+function Rockets() {
+  const rocketList = useSelector((state) => state.rockets);
+  return (
+    // <h1>Rocket</h1>
+    <Container>
+      <ul className={styles.flex1}>
+        {rocketList.map((item) => (
+          <RocketItem
+            desc={item.desc}
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            image={item.flickr_images}
+            reserved={item.reservation}
+          />
+        ))}
+      </ul>
+    </Container>
+  );
+}
+
 export default Rockets;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -6,13 +6,16 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import rocketReducer, { fetchRocketsAPI } from './rockets/rockets';
 
 import MissionsReducer from './missions/missions';
 
 const rootReducer = combineReducers({
   missions: MissionsReducer,
+  rockets: rocketReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+store.dispatch(fetchRocketsAPI());
 
 export default store;
