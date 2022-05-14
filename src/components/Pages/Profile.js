978f0e6e23bcd4eee0d5bc99d18e141aea2957f4
Regiss Mukubiza <regiss.steven@gmail.com>
2022-05-14 10:38:24 +0200

    profile rockets

--- src/components/Pages/Profile.js
+++ src/components/Pages/Profile.js
@@ -2,15 +2,23 @@ import { useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import { useSelector, useDispatch } from 'react-redux';
 import { getAllMissions } from '../../redux/missions/missions';
+import { fetchRocketsAPI } from '../../redux/rockets/rockets';
+
 import classes from './Profile.module.css';
 
 const Profile = () => {
   // @ts-ignore
   const missions = useSelector((state) => state.missions.filter((mission) => mission.joined));
   const dispatch = useDispatch();
+
+  const rockets = useSelector((state) => state.rockets.filter((rocket) => rocket.reservation));
+
   useEffect(() => {
     // @ts-ignore
-    if (!missions.length) dispatch(getAllMissions());
+    if (!missions.length && !rockets.length) {
+      dispatch(getAllMissions());
+      dispatch(fetchRocketsAPI());
+    }
   }, []);
   return (
     <section className={classes.profileContainer}>
